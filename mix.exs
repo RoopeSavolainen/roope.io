aba9cb7c96535b3aa130e4043a313e71757907ce
Roope Savolainen <roope.savolainen@gmail.com>
2020-08-01 15:40:48 +0300

    Added runtime observing to releases

--- mix.exs
+++ mix.exs
@@ -7,14 +7,20 @@ defmodule RoopeIO.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        prod: [
+          include_executables_for: [:unix],
+          steps: [:assemble, :tar],
+        ]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools, :observer, :wx],
       mod: {RoopeIO.Application, []}
     ]
   end
